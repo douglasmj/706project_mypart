76324c24893ad068ca454a0f3170f9165830244e
mariusweidmann <marius.weidmann@gmail.com>
2023-03-07 18:03:58 -0500

    added my graphs

--- streamlit_app_project.py
+++ streamlit_app_project.py
@@ -2,6 +2,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 import numpy as np
+from vega_datasets import data
 
 ### Import data ###
 
@@ -64,6 +65,119 @@ WHO_completed_series = ['DIPHCV5', 'POL3', 'MCV2', 'DTPCV3',  'RCV1', 'TT2PLUS',
 df_last_dose = df[df['ANTIGEN'].isin(WHO_completed_series)]
 
 
+
+
+###
+# for map plot
+###
+
+df_ld = df_last_dose[['NAME', 'YEAR', 'DISEASE', 'COVERAGE', 'INCIDENCE_RATE']]
+df_ld = df_ld.rename(columns={'NAME': 'Country'})
+country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/country_codes.csv', dtype = {'conuntry-code': str})
+country_df_nw = country_df.copy()
+country_df_2 = country_df_nw[['Country', 'country-code']]
+
+for_geo = df_ld.merge(country_df_2, how='inner'
+)
+for_geo = for_geo[for_geo['DISEASE'] == 'DIPHTHERIA']
+
+source = alt.topo_feature(data.world_110m.url, 'countries')
+
+#slider for year 
+year = st.slider('Year', min_value=float(df.YEAR.min()), max_value=float(df.YEAR.max()), step=1.0, format='%d')
+
+
+# filter the data based on the year selected
+for_geo = for_geo[for_geo['YEAR']==year]
+
+st.header("Global Vaccine-Preventable Disease Dashboard")
+
+width = 600
+height  = 300
+project = 'equirectangular'
+
+# a gray map using as the visualization background
+background = alt.Chart(source
+).mark_geoshape(
+    fill='#aaa',
+    stroke='white'
+).properties(
+    width=width,
+    height=height
+).project(project)
+
+######################
+# P3.4 create a selector to link two map visualizations
+selector = alt.selection_single(
+    empty='all', fields = ['Country']
+)
+
+
+# select disease through dropdown
+# make list of all disease used above
+all_disease = for_geo['DISEASE'].unique()
+
+disease_select_marius = st.selectbox('Select disease', all_disease)
+# filter the data based on the disease selected
+for_geo = for_geo[for_geo['DISEASE']==disease_select_marius]
+
+chart_base = alt.Chart(source
+    ).properties( 
+        width=width,
+        height=height
+    ).project(project
+    ).add_selection(selector
+    ).transform_lookup(
+        lookup="id",
+# Rate = COVERAGE,  Population = Incidence Rate, 
+        from_=alt.LookupData(for_geo, "country-code", ['COVERAGE', 'Country', 'INCIDENCE_RATE', 'YEAR']),
+    )
+
+# fix the color schema so that it will not change upon user selection
+coverage_scale = alt.Scale(domain=[for_geo['COVERAGE'].min(), for_geo['COVERAGE'].max()], scheme='oranges')
+coverage_color = alt.Color(field="COVERAGE:Q", type="quantitative", scale=coverage_scale)
+
+chart_coverage = chart_base.mark_geoshape().encode(
+    color=alt.Color('COVERAGE:Q', type="quantitative", scale=coverage_scale), 
+    tooltip=['COVERAGE:Q', 'Country:N']  
+    ######################
+    # P3.1 map visualization showing the mortality rate
+    # add your code here
+    # ...
+    ######################
+    # P3.3 tooltip
+    # add your code here
+    # ...
+    ).transform_filter(
+    selector
+    ).properties(
+###Need to fix the year in title?
+    title=f'Vaccine Coverage Worldwide {year}'
+)
+
+
+# fix the color schema so that it will not change upon user selection
+# incidence_scale = alt.Scale(domain=[for_geo['INCIDENCE_RATE'].min(), for_geo['INCIDENCE_RATE'].max()], scheme='yellowgreenblue')
+incidence_scale = alt.Scale(domain=[for_geo['INCIDENCE_RATE'].min(), for_geo['INCIDENCE_RATE'].max()], scheme='yellowgreenblue')
+chart_incidence = chart_base.mark_geoshape().encode(
+    color=alt.Color('INCIDENCE_RATE:Q', type="quantitative", scale=incidence_scale),
+    tooltip=['INCIDENCE_RATE:Q', 'Country:N'] 
+    ).transform_filter(
+    selector
+).properties(
+###Again the year?
+    title=f'World Disease Incidence Rate {year}'
+)
+
+chart2 = alt.vconcat(background + chart_coverage, background + chart_incidence
+).resolve_scale(
+    color='independent'
+)
+
+chart2
+
+
+
 ###
 # for bubble plot
 ####
@@ -78,8 +192,6 @@ comp_region = df_last_admin[df_last_admin.GROUP=='WHO_REGIONS']
 # create bubble plot
 ####
 
-#slider for year 
-year = st.slider('Year', min_value=float(df.YEAR.min()), max_value=float(df.YEAR.max()), step=1.0, format='%d')
 
 #subset_lastdose = df_last_admin[df_last_admin["YEAR"] == year]
 
@@ -106,6 +218,116 @@ bubble = alt.Chart(comp_region[comp_region.YEAR==year]).mark_circle().encode(
 #).configure_title(anchor='middle')
 
 #bubble
+
+
+##
+# for butterfly bar chart
+##
+
+
+# make a bar chart showing the vaccine coverage for each disease in 2018 on the left and the incidence rate on the right
+
+
+df_3 = df_ld.copy()
+df_3 = df_3[df_3['YEAR']==2018]
+df_3 = df_3[df_3['DISEASE']==disease_select_marius]
+
+#make a selection for the disease
+
+# Filter the data to include only the counties for which we have both COVERAGE and INCIDENCE_RATE data
+df_3 = df_3[df_3['COVERAGE'].notna()]
+df_3 = df_3[df_3['INCIDENCE_RATE'].notna()]
+
+
+# add brush to the chart to select a range of Countries
+country_brush = alt.selection_interval(encodings = ['y'], resolve='global')
+
+
+# make a bar chart showing the vaccine coverage for each disease in 2018
+chart3_left = alt.Chart(df_3).mark_bar(opacity=0.8).encode(
+    x=alt.X('COVERAGE', scale= alt.Scale(reverse=True)),
+    y=alt.Y('Country', axis = None, sort=alt.EncodingSortField(field="COVERAGE", op="sum", order="descending"))
+).transform_filter(
+    country_brush
+).properties(
+    width=300,
+    height=750
+)
+
+
+chart3_right = alt.Chart(df_3).mark_bar(opacity=0.8, color='red').encode(
+    x=alt.X('INCIDENCE_RATE', scale= alt.Scale(reverse=False)) ,
+    y=alt.Y('Country', sort=alt.EncodingSortField(field="COVERAGE", op="sum", order="descending")),
+).add_selection(
+    country_brush
+).transform_filter(
+    country_brush
+).properties(
+    width=300,
+    height=750
+)
+
+chart3 = alt.hconcat(chart3_left, chart3_right).properties(
+    title='Vaccine Coverage and Disease Incidence Rate in 2018'
+).resolve_scale(
+    y = 'shared'
+)
+
+chart3
+
+# end of butterfly bar chart
+
+##
+# Illustrate change in vaccination coverage and disease incidence over time using a line chart
+##
+
+
+
+df_5 = df_ld.copy()
+# for each year and disease, get the mean coverage and incidence rate
+df_5 = df_5.groupby(['YEAR', 'DISEASE']).mean().reset_index()
+
+disease_selection_3 = alt.selection_single(
+    fields=['DISEASE'], bind = "legend"
+)
+
+
+# make an altair chart with the Year on the x-axis and the COVERAGE on the y-axis with different colors for each disease
+
+chart5_1 = alt.Chart(df_5).mark_line().encode(
+    x= alt.X('YEAR', title='Year', scale=alt.Scale(domain=(1980, 2021))),
+    y='COVERAGE',
+    color='DISEASE'
+).add_selection(
+    disease_selection_3
+).transform_filter(
+    disease_selection_3
+).properties(
+    title='Vaccine Coverage Over Time',
+    width=700,
+    height=350
+)
+# make another chart that shows the INCIDENCE_RATE over time
+chart5_2 = alt.Chart(df_5).mark_line().encode(
+    x= alt.X('YEAR', title='Year', scale=alt.Scale(domain=(1980, 2021))),
+    y='INCIDENCE_RATE',
+    color='DISEASE'
+).transform_filter(
+    disease_selection_3
+).properties(
+    title='Disease Incidence Rate Over Time',
+    width=700,
+    height=350
+)
+
+
+# combine the two charts into one chart showing one chart above the other
+chart5 = alt.vconcat(chart5_1, chart5_2)
+chart5
+
+
+# end of line chart
+
 
 
 
